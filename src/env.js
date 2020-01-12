a1c7e957e33c30982b86a37b61a0841f73523893
mhy624 <mhy624@163.com>
2020-01-12 14:12:43 +0800

    头部组件

--- src/env.js
+++ src/env.js
@@ -2,18 +2,18 @@ let baseURL
 switch (process.env.NODE_ENV) {
     // 开发
     case 'dev':
-        baseURL = 'http:/dev-mall-pre.springboot.cn/api'
+        baseURL = 'http://dev-mall-pre.springboot.cn/api'
         break;
     // 测试
     case 'test':
-        baseURL = 'http:/test-mall-pre.springboot.cn/api'
+        baseURL = 'http://test-mall-pre.springboot.cn/api'
         break;
     // 线上
     case 'prod':
-        baseURL = 'http:/mall-pre.springboot.cn/api'
+        baseURL = 'http://mall-pre.springboot.cn/api'
         break;
     default:
-        baseURL = 'http:/mall-pre.springboot.cn/api'
+        baseURL = 'http://mall-pre.springboot.cn/api'
         break;
 }
 
