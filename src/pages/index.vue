d3d1e3c538e6d57aec0d46145680e80e0f7845be
mhy624 <mhy624@163.com>
2020-01-15 19:45:43 +0800

    modal动画

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -88,7 +88,7 @@
                                 <div class="item-info">
                                     <h3>{{item.name}}</h3>
                                     <p>{{item.subtitle}}</p>
-                                    <p class="price">{{item.price}}元</p>
+                                    <p class="price" @click="addCart(item.id)">{{item.price}}元</p>
                                 </div>
                             </div>
                         </div>
@@ -103,7 +103,9 @@
             sureText="查看购物车" 
             btnType="1" 
             modalType="midddle"
-            :showModal=true>
+            :showModal="showModal"
+            @submit="goToCart"
+            @cancel="showModal=false">
         <template v-slot:body>
             <p>商品添加成功！</p>
         </template>
@@ -215,7 +217,8 @@ export default {
                     img:'/imgs/ads/ads-4.jpg'
                 },
             ],
-            phoneList:[]
+            phoneList:[],
+            showModal:false
         }
     },
     mounted(){
@@ -232,8 +235,21 @@ export default {
                 res.list = res.list.slice(6,14)
                 this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)]
                 
-            })
-            
+            })           
+        },
+        addCart(){
+            this.showModal = true;
+            // this.axios.post('/carts',{
+            //     productId:id,
+            //     selected:true
+            // }).then(()=>{
+
+            // }).catch(()=>{
+            //     this.showModal = true;
+            // })
+        },
+        goToCart(){
+            this.$router.push('/cart')
         }
     }
 }
